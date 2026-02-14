062166dfc0c2693dad9c66612a0850bc0851f9b3
Ricardo Martins <ricardo.martins.6373@gmail.com>
2026-02-14 09:33:23 -0300

    feature: Criando um separador visual

---
+++ app/_components/separator/Separator.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import clsx from "clsx";
+
+type SeparatorBaseProps = React.ComponentPropsWithoutRef<"div">;
+
+type SeparatorComponent = React.FC<SeparatorBaseProps>;
+
+export const Separator: SeparatorComponent = ({ className }) => {
+  return <div className={clsx("border-b border-gray-300", className)}></div>;
+}
